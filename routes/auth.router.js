d1013961ca48612b53ff5850fd6c263a91ef4ccd
utaridyani <dyanilaksmi28@gmail.com>
2022-11-20 07:48:33 +0800

    middlewares

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,5 +1,6 @@
-const express = require("express"),
+var express = require("express"),
   router = express.Router(),
+  verifyToken = require('../middlewares/auth'),
   {
     signin
   } = require("../controllers/auth.controller.js");
@@ -8,5 +9,20 @@ router.post("/login", signin, function (req, res) {
 
 });
 
+//ini sintaks yang bisa dipakai untuk verifikasi admin / user supaya bisa bedakan aksesnya
+router.get("/hiddencontent", verifyToken, function (req, res) {
+  const role = req.user.role;
+  if (role == "admin") {
+    res.status(200)
+      .send({
+        message: "Congratulations! but there is no hidden content"
+      });
+  } else {
+    res.status(403)
+      .send({
+        message: "Unauthorised access"
+      });
+  }
+});
 
 module.exports = router;
