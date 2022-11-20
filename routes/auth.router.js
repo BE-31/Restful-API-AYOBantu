9500d034bc2fb6b4be60e88af9375caf6d9199f4
Utari Dyani Laksmi <74400122+utaridyani@users.noreply.github.com>
2022-11-20 17:05:44 +0800

    Update auth.router.js

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -9,20 +9,5 @@ router.post("/login", signin, function (req, res) {
 
 });
 
-//ini sintaks yang bisa dipakai untuk verifikasi admin / user supaya bisa bedakan aksesnya
-router.get("/hiddencontent", verifyToken, function (req, res) {
-  const role = req.user.role;
-  if (role == "admin") {
-    res.status(200)
-      .send({
-        message: "Congratulations! but there is no hidden content"
-      });
-  } else {
-    res.status(403)
-      .send({
-        message: "Unauthorised access"
-      });
-  }
-});
 
 module.exports = router;
